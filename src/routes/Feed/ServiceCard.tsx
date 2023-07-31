4c51a55a138a86780165937a47079aac34dd322d
tkpepper15 <97776629+tkpepper15@users.noreply.github.com>
2023-07-31 17:50:46 -0400

    Update ServiceCard.tsx

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -9,7 +9,7 @@ const ServiceCard: React.FC = () => {
   return (
     <>
       <StyledTitle>
-        <Emoji>🌟</Emoji> Service
+        Contact
       </StyledTitle>
       <StyledWrapper>
         {CONFIG.projects.map((project, idx) => (
