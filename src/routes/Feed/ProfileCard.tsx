6a8aaf5283aef30e76bce6ace0488c1f8f9d9603
tkpepper15 <97776629+tkpepper15@users.noreply.github.com>
2023-07-31 17:51:35 -0400

    Update ProfileCard.tsx

--- src/routes/Feed/ProfileCard.tsx
+++ src/routes/Feed/ProfileCard.tsx
@@ -10,7 +10,7 @@ const ProfileCard: React.FC<Props> = () => {
   return (
     <StyledWrapper>
       <div className="title">
-        <Emoji>💻</Emoji> Profile
+        Profile
       </div>
       <div className="content">
         <div className="top">
