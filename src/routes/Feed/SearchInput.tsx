bbb47c0659d62019f7978922f6dc76f2b5c29298
tkpepper15 <97776629+tkpepper15@users.noreply.github.com>
2023-07-31 18:51:59 -0700

    Update SearchInput.tsx

--- src/routes/Feed/SearchInput.tsx
+++ src/routes/Feed/SearchInput.tsx
@@ -8,6 +8,7 @@ const SearchInput: React.FC<Props> = ({ ...props }) => {
   return (
     <StyledWrapper>
       <div className="top">
+        <Emoji>🔎</Emoji> Search
       </div>
       <input
         className="mid"
@@ -39,5 +40,6 @@ const StyledWrapper = styled.div`
     border-radius: 1rem;
     outline-style: none;
     width: 100%;
-    background-color: ${({ theme }) => theme.scheme === "light" ? "white" : theme.colors.gray4};
+    background-color: ${({ theme }) => theme.colors.gray4};
+  }
 `
