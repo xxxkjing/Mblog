7ee0d6df55e6f1c0a42119a3f7b5b4feeb6c431a
tkpepper15 <97776629+tkpepper15@users.noreply.github.com>
2023-08-21 20:13:44 -0400

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "tejjas kaul",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "HS Student | Neuro",
-    bio: "This little nook on the internet displays what I've learned about our brain and the tech surrounding it.",
+    bio: "This little nook on the internet showcases what I've learned about the brain and the tech surrounding it.",
     email: "tejjas15@gmail.com",
     linkedin: "Tejjas Kaul",
     github: "tkpepper15",
