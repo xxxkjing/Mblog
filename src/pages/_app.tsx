0ef04ebfcd711f6944810a3270fdfd5d9d82f804
tejjas kaul <97776629+tkpepper15@users.noreply.github.com>
2023-10-21 19:31:09 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,25 @@
-import { AppPropsWithLayout } from "../types"
-import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
-import { RootLayout } from "src/layouts"
-import { queryClient } from "src/libs/react-query"
+import { AppPropsWithLayout } from "../types";
+import { Hydrate, QueryClientProvider } from "@tanstack/react-query";
+import { RootLayout } from "src/layouts";
+import { queryClient } from "src/libs/react-query";
+import { ThemeProvider } from "@emotion/react"; // Import ThemeProvider
+import { createTheme } from "../styles/theme"; // Import your theme configuration
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout || ((page) => page)
+  const getLayout = Component.getLayout || ((page) => page);
+
+  const theme = createTheme({ scheme: "light" }); // Create your theme
 
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
+        <ThemeProvider theme={theme}>
+          <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
+        </ThemeProvider>
       </Hydrate>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
+
