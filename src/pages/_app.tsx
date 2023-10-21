135107115e8f0b8ed04cad07d8c93f300c1320ba
tejjas kaul <97776629+tkpepper15@users.noreply.github.com>
2023-10-21 19:40:52 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,20 @@ import { AppPropsWithLayout } from "../types";
 import { Hydrate, QueryClientProvider } from "@tanstack/react-query";
 import { RootLayout } from "src/layouts";
 import { queryClient } from "src/libs/react-query";
-import { ThemeProvider } from "@emotion/react"; // Import ThemeProvider
-import { createTheme } from "../styles/theme"; // Import your theme configuration
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "../styles/theme";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons"; // Import FontAwesome icons
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page);
 
   const theme = createTheme({ scheme: "light" }); // Create your theme
+
+  // Initialize the FontAwesome library with the icons
+  library.add(faSun, faMoon);
+
+  // Use the FontAwesome icons for light and dark modes
+  theme.modeIcon = theme.scheme === "light" ? faSun : faMoon;
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -22,4 +29,3 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
 }
 
 export default App;
-
