b511b840c36b31ee49603a852e3abf6fe199b81f
tejjas kaul <97776629+tkpepper15@users.noreply.github.com>
2023-10-21 19:59:14 -0400

    Create Icon.jsx

---
+++ src/components/Icon.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Icon from './Icon'; // Adjust the import path as needed
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+
+function MyComponent() {
+  return (
+    <div>
+      <Icon icon={faSun} size="2x" color="orange" />
+      <Icon icon={faMoon} size="2x" color="gray" />
+      {/* Other content */}
+    </div>
+  );
+}
+
+export default MyComponent;
