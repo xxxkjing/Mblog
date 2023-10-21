a2be5fba752dbfb0c99c3d39730cf4d5a33bdd20
tejjas kaul <97776629+tkpepper15@users.noreply.github.com>
2023-10-21 18:38:33 -0400

    Update theme.ts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,26 +1,35 @@
-import { Theme } from "@emotion/react"
-import { Colors, colors } from "./colors"
-import { variables } from "./variables"
-import { zIndexes } from "./zIndexes"
+import { Theme } from "@emotion/react";
+import { Colors, colors } from "./colors";
+import { variables } from "./variables";
+import { zIndexes } from "./zIndexes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Example import for FontAwesome icons
 
 declare module "@emotion/react" {
   export interface Theme {
-    scheme: Scheme
-    colors: Colors
-    zIndexes: typeof zIndexes
-    variables: typeof variables
+    scheme: Scheme;
+    colors: Colors;
+    zIndexes: typeof zIndexes;
+    variables: typeof variables;
   }
 }
 
-export type Scheme = "light" | "dark"
+export type Scheme = "light" | "dark";
 
 type Options = {
-  scheme: Scheme
-}
+  scheme: Scheme;
+};
 
 export const createTheme = (options: Options): Theme => ({
   scheme: options.scheme,
   colors: colors[options.scheme],
   variables: variables,
   zIndexes: zIndexes,
-})
+});
+
+// Define icons for light and dark modes
+const lightIcon = <FontAwesomeIcon icon="sun" />; // Example FontAwesome icon for light mode
+const darkIcon = <FontAwesomeIcon icon="moon" />; // Example FontAwesome icon for dark mode
+
+export const getModeIcon = (scheme: Scheme) => {
+  return scheme === "light" ? lightIcon : darkIcon;
+};
