c38ec6fe2b6fdc5067be5a758edb6560bb6e7b3d
tejjas kaul <97776629+tkpepper15@users.noreply.github.com>
2023-10-21 20:03:30 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
-import { AppPropsWithLayout } from "../types";
+import React from "react";
+import { AppProps } from "next/app";
 import { Hydrate, QueryClientProvider } from "@tanstack/react-query";
-import { RootLayout } from "src/layouts";
-import { queryClient } from "src/libs/react-query";
 import { ThemeProvider } from "@emotion/react";
+import { queryClient } from "src/libs/react-query";
 import { createTheme } from "../styles/theme";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons"; // Import FontAwesome icons
+import { library } from "@fortawesome/fontawesome-svg-core"; // Import the library
 
 // Initialize FontAwesome library with the icons
 library.add(faSun, faMoon);
 
-function App({ Component, pageProps }: AppPropsWithLayout) {
+function MyApp({ Component, pageProps }: AppProps) {
   // Create your theme based on the selected scheme
   const theme = createTheme({ scheme: "light" });
 
@@ -24,12 +24,13 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
         <ThemeProvider theme={theme}>
-          <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
+          <Component {...pageProps} />
         </ThemeProvider>
       </Hydrate>
     </QueryClientProvider>
   );
 }
 
-export default App;
+export default MyApp;
+
 
