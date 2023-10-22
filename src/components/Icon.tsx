38cad2091e77dffb62d977dedd928e6724756dcb
tejjas kaul <97776629+tkpepper15@users.noreply.github.com>
2023-10-21 20:01:39 -0400

    Update and rename Icon.jsx to Icon.tsx

---
+++ src/components/Icon.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
+type Props = {
+  icon: IconDefinition; // FontAwesome icon definition
+  className?: string;
+  children?: ReactNode;
+  size?: string; // FontAwesome size (e.g., 'lg', '2x', '3x', 'xs', etc.)
+  color?: string; // FontAwesome icon color
+};
+
+const Icon = ({ icon, className, children, size = "lg", color = "currentColor" }: Props) => {
+  return (
+    <FontAwesomeIcon icon={icon} size={size} color={color} className={className}>
+      {children}
+    </FontAwesomeIcon>
+  );
+};
+
+export default Icon;
