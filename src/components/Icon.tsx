2a26cc6cf62d315d0ed5d4c49e6331b72c5394f6
tejjas kaul <97776629+tkpepper15@users.noreply.github.com>
2023-11-08 22:32:42 -0500

    Update Icon.tsx

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,17 +1,1 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { IconDefinition, SizeProp } from '@fortawesome/fontawesome-svg-core';
 
-interface IconProps {
-  icon: IconDefinition;
-  size?: SizeProp;  // Use SizeProp type here instead of string
-  color?: string;
-}
-
-const Icon: React.FC<IconProps> = ({ icon, size, color }) => {
-  // Since FontAwesomeIcon expects size to be SizeProp or undefined,
-  // we cast size to SizeProp if it's not undefined.
-  return <FontAwesomeIcon icon={icon} size={size as SizeProp | undefined} color={color} />;
-};
-
-export default Icon;
