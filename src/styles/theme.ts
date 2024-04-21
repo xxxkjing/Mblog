d1edda010199fc607b3ecd8bdcd439bfb4ea6244
Tejjas Kaul <tejjaskaul@Tejjass-MacBook-Pro.local>
2024-04-21 11:06:59 -0400

    Commit message

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,9 +2,6 @@ import { Theme } from "@emotion/react";
 import { Colors, colors } from "./colors";
 import { variables } from "./variables";
 import { zIndexes } from "./zIndexes";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { IconDefinition, library } from "@fortawesome/fontawesome-svg-core";
-import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 declare module "@emotion/react" {
   export interface Theme {
@@ -12,7 +9,6 @@ declare module "@emotion/react" {
     colors: Colors;
     zIndexes: typeof zIndexes;
     variables: typeof variables;
-    modeIcon: IconDefinition;
   }
 }
 
@@ -20,16 +16,4 @@ export type Scheme = "light" | "dark";
 
 type Options = {
   scheme: Scheme;
-};
-
-export const createTheme = (options: Options): Theme => {
-  library.add(faSun, faMoon); // Add FontAwesome icons to the library
-
-  return {
-    scheme: options.scheme,
-    colors: colors[options.scheme],
-    variables: variables,
-    zIndexes: zIndexes,
-    modeIcon: options.scheme === "light" ? faSun : faMoon,
-  };
 };
