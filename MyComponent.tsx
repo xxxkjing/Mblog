a09268870207d8d4185b374492c0af7f2044a3ae
Tejjas Kaul <tejjaskaul@Tejjass-MacBook-Pro.local>
2024-04-21 00:38:16 -0400

    Commit message

--- MyComponent.tsx
+++ MyComponent.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
-
 const MyComponent: React.FC = () => {
   // Access environmental variables
   const utterancesRepo = process.env.NEXT_PUBLIC_UTTERANCES_REPO;
   const notionPageId = process.env.NOTION_PAGE_ID;
 
   // Use FontAwesome icons
-  const icon = process.env.THEME === 'light' ? faSun : faMoon;
 
   return (
     <div>
