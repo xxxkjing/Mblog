e6ac713e1c43d822b8c068e55023d8b3a5247842
Tejjas Kaul <tejjaskaul@Tejjass-MacBook-Pro.local>
2024-04-20 23:56:22 -0400

    Commit message

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// next.config.js
 module.exports = {
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
@@ -5,5 +6,19 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'static/fonts',
+            publicPath: '../fonts', // Adjust this path based on your project structure
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
-
