126ba9e371ba33171e3d7a596f0c37983da7ede0
Tejjas Kaul <tejjaskaul@Tejjass-MacBook-Pro.local>
2024-04-21 21:14:42 -0400

    Commit message

--- src/layouts/IndexLayout.tsx
+++ src/layouts/IndexLayout.tsx
@@ -82,7 +82,7 @@ const AboutMe = styled.div`
   border-radius: 10px;
   box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
 
-  @media (max-width: 400px) {
+  @media (max-width: 500px) {
     width: calc(100% - 1px);
     max-width: 400px;
     left: 0;
