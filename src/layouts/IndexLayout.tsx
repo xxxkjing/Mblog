99d65caea9b416af879ce2b9c4339788ff7fef67
TKpepper15 <tejjas15@gmail.com>
2024-07-24 00:44:44 -0400

    Commit message

--- src/layouts/IndexLayout.tsx
+++ src/layouts/IndexLayout.tsx
@@ -2,8 +2,12 @@ import React, { ReactNode, useEffect } from "react";
 import { useRouter } from "next/router";
 import styled from "@emotion/styled";
 import Image from "next/image";
-import { FiFile } from "react-icons/fi";
+import { FiFile, FiLink } from "react-icons/fi";
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 type LayoutProps = {
   children: ReactNode;
@@ -17,14 +21,47 @@ const IndexLayout: React.FC<LayoutProps> = ({ children }) => {
     console.log("Current Path:", router.pathname);
   }, [router.pathname]);
 
+  const settings = {
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000, // Adjust the autoplay speed
+    speed: 500,
+    fade: true, // Enable fade effect
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <FaChevronRight />,
+    prevArrow: <FaChevronLeft />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  };
+
   return (
     <Container>
       {isIndexPage && (
         <Header>
           <ImageContainer>
-            <Image src="/images/handwave.png" alt="Tejjas Kaul" width={200} height={200} />
+            <Image
+              src="/images/handwave.png"
+              alt="Tejjas Kaul"
+              width={200}
+              height={200}
+            ></Image>
             <AboutMe>
-            <p>Hi, I'm <strong>Tejjas Kaul</strong>! I'm a high schooler who is keen on exploring neurotech and the policies at play. Check out the <LinkStyle href="/blog">blog</LinkStyle> and the links below to stay updated:</p>
+              <p>Hi, I'm <strong>Tejjas Kaul</strong>! I'm a high schooler who is keen on exploring neurotech, public health, and design. Check out the <LinkStyle href="/blog">blog</LinkStyle> and the links below to stay updated:</p>
             </AboutMe>
             <SocialLinks>
               <a href="https://docs.google.com/document/d/1SVg5OicX0dVmVkmRItPTlU5I_I7bLPGrKWgzEr2HdlA/edit?usp=sharing" target="_blank" rel="noopener noreferrer"><FiFile /></a>
@@ -35,7 +72,35 @@ const IndexLayout: React.FC<LayoutProps> = ({ children }) => {
           </ImageContainer>
         </Header>
       )}
-      <Main>{children}</Main>
+      <Main>
+        <MainContainer>
+          <Heading>
+            <br></br>
+            Relevant Projects
+          </Heading>
+          <Carousel {...settings}>
+            <MyProjects>
+              <RoundedImage
+                src="/images/dukeappliedethics.png"
+                alt="Duke Applied Ethics+ Project"
+                width={500}
+                height={300}
+              />
+              <p>Case Study Analysis, User Research, Video Editing, WebDev</p>
+              <StyledLink href="https://applied-ethics.vercel.app/" target="_blank" rel="noopener noreferrer"><FiLink />Duke Applied Ethics+ & NCSSM SRIP</StyledLink>
+            </MyProjects>
+            <MyProjects>
+              <StyledLink href="https://drive.google.com/file/d/10IFe0ovxQgVi8ZPEvog8hrw5WO1sEgcC/view?usp=sharing" target="_blank" rel="noopener noreferrer"><FiLink />Neuroethics Scientific Essay</StyledLink>
+            </MyProjects>
+            <MyProjects>
+              <StyledLink href="https://tkpepper15.github.io/neuro-midterm/" target="_blank" rel="noopener noreferrer"><FiLink />Neuro-Ophthalmology Guide</StyledLink>
+            </MyProjects>
+            <MyProjects>
+              <StyledLink href="https://yci-website-enterprise.vercel.app/" target="_blank" rel="noopener noreferrer"><FiLink />Youth Climate Initiative Website</StyledLink>
+            </MyProjects>
+          </Carousel>
+        </MainContainer>
+      </Main>
     </Container>
   );
 };
@@ -49,7 +114,7 @@ const Container = styled.div`
 const Header = styled.header`
   max-width: 800px;
   margin: 0 auto;
-  padding: 2rem;
+  padding: 3rem;
   text-align: center;
 `;
 
@@ -60,9 +125,23 @@ const ImageContainer = styled.div`
   align-items: center;
 `;
 
+const RoundedImage = styled(Image)`
+  border-radius: .5rem;
+  overflow: hidden;
+  width: 100%;
+  height: auto;
+`;
+
+const MainContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 const SocialLinks = styled.div`
   position: absolute;
-  bottom: -50px; /* Adjust this value as needed */
+  bottom: -50px;
   display: flex;
   gap: 20px;
   background-color: ${({ theme }) =>
@@ -72,15 +151,46 @@ const SocialLinks = styled.div`
   box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.1);
 `;
 
+const MyProjects = styled.div`
+  display: flex;
+  padding: 1rem;
+  width: 25rem;
+  flex-direction: column;
+  border-radius: 10px;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+  margin: 0 auto;
+  background: ${({ theme }) =>
+    theme.scheme === "light" ? "white" : theme.colors.gray1};
+  transition: background 0.3s ease, color 0.3s ease;
+
+  &:hover {
+    background: black;
+    color: white;
+  }
+`;
+
+const StyledLink = styled.a`
+  font-weight: bold;
+  text-decoration: underline;
+  font-size: 1.75rem;
+  color: inherit;
+`;
+
 const LinkStyle = styled.a`
   color: gray;
+`;
+
+const Heading = styled.h2`
+  font-size: 1.75rem;
+  line-height: 2.5rem;
+  font-weight: 700;
 `;
 
 const AboutMe = styled.div`
   display: flex;
   padding: 0.25rem;
   flex-direction: column;
-  border-radius: 10px 10px 10px 10px;
+  border-radius: 10px;
   box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
   background-color: ${({ theme }) =>
     theme.scheme === "light" ? "white" : theme.colors.gray1};
@@ -88,6 +198,28 @@ const AboutMe = styled.div`
 
 const Main = styled.main`
   flex: 1;
+`;
+
+const Carousel = styled(Slider)`
+  width: 80%;
+
+  .slick-slide {
+    display: flex;
+    justify-content: center;
+    padding: 10px;
+  }
+
+  .slick-prev, .slick-next {
+    color: ${({ theme }) => theme.scheme === "light" ? "black" : "white"};
+  }
+
+  .slick-prev:before, .slick-next:before {
+    font-family: 'FontAwesome';
+  }
+
+  .slick-dots li button:before {
+    color: ${({ theme }) => theme.scheme === "light" ? "black" : "white"};
+  }
 `;
 
 export default IndexLayout;
