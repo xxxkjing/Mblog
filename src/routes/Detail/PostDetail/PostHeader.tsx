b4471e32ff3a8f71a3f68de3916d0e4230620e3a
TKpepper15 <tejjas15@gmail.com>
2025-01-14 16:15:18 -0500

    Commit message

--- src/routes/Detail/PostDetail/PostHeader.tsx
+++ src/routes/Detail/PostDetail/PostHeader.tsx
@@ -6,58 +6,34 @@ import Image from "next/image"
 import React from "react"
 import styled from "@emotion/styled"
 
-type Props = {
-  data: TPost
+interface PostHeaderProps {
+  data: {
+    title: string;
+    type?: string[];
+    author?: Array<{ name: string }>;
+    date?: { start_date: string };
+    createdTime: string;
+  }
 }
 
-const PostHeader: React.FC<Props> = ({ data }) => {
+const PostHeader: React.FC<PostHeaderProps> = ({ data }) => {
   return (
     <StyledWrapper>
       <h1 className="title">{data.title}</h1>
-      {data.type[0] !== "Paper" && (
+      {data.type?.[0] !== "Paper" && (
         <nav>
           <div className="top">
-            {data.author && data.author[0] && data.author[0].name && (
-              <>
-                <div className="author">
-                  <Image
-                    css={{ borderRadius: "50%" }}
-                    src={data.author[0].profile_photo || CONFIG.profile.image}
-                    alt="profile_photo"
-                    width={24}
-                    height={24}
-                  />
-                  <div className="">{data.author[0].name}</div>
-                </div>
-                <div className="hr"></div>
-              </>
+            {data.author?.[0]?.name && (
+              <div className="author">{data.author[0].name}</div>
             )}
             <div className="date">
-              {formatDate(
-                data?.date?.start_date || data.createdTime,
-                CONFIG.lang
-              )}
+              {new Date(data.date?.start_date || data.createdTime).toLocaleDateString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+              })}
             </div>
           </div>
-          <div className="mid">
-            {data.tags && (
-              <div className="tags">
-                {data.tags.map((tag: string) => (
-                  <Tag key={tag}>{tag}</Tag>
-                ))}
-              </div>
-            )}
-          </div>
-          {data.thumbnail && (
-            <div className="thumbnail">
-              <Image
-                src={data.thumbnail}
-                css={{ objectFit: "cover" }}
-                fill
-                alt={data.title}
-              />
-            </div>
-          )}
         </nav>
       )}
     </StyledWrapper>
